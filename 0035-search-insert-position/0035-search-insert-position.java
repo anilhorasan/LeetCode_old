3b2792730a90274f6502ade49bb73593d0bf21bb
Anil Horasan <anilhorasan@gmail.com>
2023-04-26 11:30:06 +0300

    Time: 0 ms (100.00%), Space: 41.7 MB (98.15%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -1,12 +1,25 @@
 class Solution {
     public int searchInsert(int[] nums, int target){
+        
+        int left = 0;
+        int right = nums.length - 1;
+
+        while(left <= right){
+           int mid = left + (right-left) / 2;
+           if(target < nums[mid]) right = mid - 1;
+           else if(target > nums[mid]) left = mid + 1;
+           else return mid;
+        }
+        return left;
+        
+    }
+    
+    public int searchInsert2(int[] nums, int target){
         int i;
         for(i = 0; i < nums.length; i++){
             if(nums[i] == target)    return i;
             if(nums[i] > target)    return i;
-            
         }
-        
         return i;
     }
 }
