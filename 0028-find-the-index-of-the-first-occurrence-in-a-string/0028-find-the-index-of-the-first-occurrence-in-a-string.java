c1d7a2d45a0a5b264a7adf58ca2e779306f0c26c
Anil Horasan <anilhorasan@gmail.com>
2023-04-26 10:42:01 +0300

    Time: 1 ms (39.31%), Space: 40.9 MB (19.94%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,20 +1,7 @@
 class Solution {
+    // Runtime: 0 ms
+    // Memory: 40.7 MB
     public int strStr2(String haystack, String needle) {
-        int len = needle.length();
-        Map<String, Integer> map = new HashMap<>();
-        for(int i = 0; i < haystack.length() - len + 1; i++){
-            map.put(haystack.substring(i,len+i), i);
-            if(map.containsKey(needle))
-                return map.get(needle);
-            
-        }
-        for (Map.Entry<String, Integer> entry : map.entrySet()) 
-            System.out.println(entry.getKey() + ":" + entry.getValue().toString());
-
-        return -1;
-    }
-    
-    public int strStr(String haystack, String needle) {
         int m = needle.length();
         int n = haystack.length();
 
@@ -31,4 +18,20 @@ class Solution {
 
         return -1;
     }
+    
+    // Runtime: 16 ms
+    // Memory: 42.4 MB
+    public int strStr(String haystack, String needle) {
+        int len = needle.length();
+        Map<String, Integer> map = new HashMap<>();
+        for(int i = 0; i < haystack.length() - len + 1; i++){
+            map.put(haystack.substring(i,len+i), i);
+            if(map.containsKey(needle))
+                return map.get(needle);
+            
+        }
+        return -1;
+    }
+    
+    
 }
