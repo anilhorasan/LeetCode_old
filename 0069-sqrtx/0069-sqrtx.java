6b934ecdb9b0152b50adc292cae602f0f8f277df
Anil Horasan <anilhorasan@gmail.com>
2023-04-27 15:19:05 +0300

    Time: 21 ms (14.80%), Space: 40.3 MB (12.60%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int mySqrt3(int x) {
+        if (x == 0 || x == 1) return x;
+        int left = 0;
+        int right = x;
+        int mid = 0;
+        while (left <= right){
+            mid = left + (right - left) / 2;
+            if(mid  < x / mid)      left  = mid + 1;
+            else if(mid > x / mid)  right = mid - 1;
+            else                    return  mid;
+        }
+        return right;
+    }
+
+    // Newton's formula
+    public int mySqrt2(int x) {
+        long r = x;
+        while (r*r > x)
+            r = (r + x/r) / 2;
+        return (int) r;
+    }
+    // teest case: 2147395599
+    
+    public int mySqrt(int x){
+        
+        long result = 0;
+        while (result*result <= x)   result++;
+        return (int)result-1;
+    }
+    
+}
